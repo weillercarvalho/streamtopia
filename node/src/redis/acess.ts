38f658f76d937ba923a82ce8cc3acdc1bbd36e10
Weiller Carvalho <engweiller@hotmail.com>
2023-02-23 10:00:35 -0300

    :hammer:: Some issues fixed.

--- node/src/redis/acess.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "redis";
-
-const client = createClient();
-
-client.on("error", (err) => console.log("Redis Client Error", err));
-
-await client.connect();
-
-export default client;
