79cd0d8acfb7015badcbdb230201581cce7b0797
Weiller Carvalho <engweiller@hotmail.com>
2023-03-09 03:19:54 -0300

    docs: Change in package.json

--- frontend/src/Services/Services.jsx
+++ frontend/src/Services/Services.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'http://localhost:5000' // Change this for the IP Address in the Docker EC2 Machine.;
 
 
 function creatingHeaders() {
